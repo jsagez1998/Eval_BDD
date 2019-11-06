fee787d8c555c15abe0c5c7f79ff59ca6be58704
Jason <51094681+jsagez1998@users.noreply.github.com>
2019-11-06 10:37:47 +0100

    Add files via upload

--- eval_bdd_exo1.sql
+++ eval_bdd_exo1.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS clients (
   cli_tel varchar(30) DEFAULT NULL,
   PRIMARY KEY (cli_num),
   KEY IX_nom (cli_nom)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
 -- Listage de la structure de la table eval_bdd. commande
 CREATE TABLE IF NOT EXISTS commande (
@@ -20,8 +20,8 @@ CREATE TABLE IF NOT EXISTS commande (
   com_obs varchar(50) DEFAULT NULL,
   PRIMARY KEY (com_num),
   KEY cli_num (cli_num),
-  CONSTRAINT commande_ibfk_1 FOREIGN KEY (cli_num) REFERENCES client (cli_num)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  commande_ibfk_1 FOREIGN KEY (cli_num) REFERENCES client (cli_num)
+);
 
 
 -- Listage de la structure de la table eval_bdd. est_compose
@@ -31,9 +31,9 @@ CREATE TABLE IF NOT EXISTS est_compose (
   est_qte int(11) DEFAULT NULL,
   PRIMARY KEY (com_num,pro_num),
   KEY pro_num (pro_num),
-  CONSTRAINT est_compose_ibfk_1 FOREIGN KEY (com_num) REFERENCES commande (com_num),
-  CONSTRAINT est_compose_ibfk_2 FOREIGN KEY (pro_num) REFERENCES produit (pro_num)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  est_compose_ibfk_1 FOREIGN KEY (com_num) REFERENCES commande (com_num),
+  est_compose_ibfk_2 FOREIGN KEY (pro_num) REFERENCES produit (pro_num)
+);
 
 -- Listage de la structure de la table eval_bdd. produit
 CREATE TABLE IF NOT EXISTS produit (
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS produit (
   pro_libelle varchar(50) DEFAULT NULL,
   pro_description varchar(50) DEFAULT NULL,
   PRIMARY KEY (pro_num)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
 -- Création de l'index sur cli_nom
 DROP INDEX IF EXISTS IX_nom ON clients;
